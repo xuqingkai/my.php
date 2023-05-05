cc03a67f7a55dff5fd8275b54421e03c3a3a0d90
徐清凯 <id@xuqingkai.com>
2023-05-05 16:26:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-cn" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8">
+    <title>xuqingkai.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+   phpinfo(); 
+?>
+</body>
+</html>
