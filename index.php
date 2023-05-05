47f6de4150f1c0d2ef14dbc15f336f55bc82809d
徐清凯 <id@xuqingkai.com>
2023-05-05 16:30:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="zh-cn" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="utf-8">
-    <title>xuqingkai.com</title>
+    <meta charset="utf-8">
+    <title>php</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
